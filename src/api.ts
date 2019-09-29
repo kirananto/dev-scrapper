0b12b4984b07ba477407e5ea1da521e5699f81b7
Kiran Anto <kirananto@gmail.com>
2019-09-29 09:05:23 +0530

    Add cors support for the apis

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,10 @@
 import chalk from "chalk"
 import { getKeywords, getRepos, getEmailList } from "./models"
+var cors = require('cors')
 
 const express = require('express')
 const app = express()
+app.use(cors())
  
 app.get('/keywords', function (req, res) {
     getKeywords().then(result => {
@@ -12,34 +14,34 @@ app.get('/keywords', function (req, res) {
 
 app.get('/keywords/:keyword', function (req, res) {
     getKeywords({ keyword: req.params.keyword}).then(result => {
-        res.send(result)
+        res.json(result)
     })
 })
 
 app.get('/keywords/:keyword/repos', function (req, res) {
     getRepos({ keyword: req.params.keyword}).then(result => {
-        res.send(result)  
+        res.json(result)  
     })
 })
 
 app.get('/keywords/:keyword/repos/:repoId', function (req, res) {
     getRepos({ keyword: req.params.keyword, repoId: req.params.repoId }).then(result => {
-        res.send(result)  
+        res.json(result)  
     })
 })
 
 app.get('/keywords/:keyword/repos/:repoId', function (req, res) {
     getEmailList({ keyword: req.params.keyword, repoId: req.params.repoId }).then(result => {
-        res.send(result)  
+        res.json(result)  
     })
 })
 
 app.get('/keywords/:keyword/emails/', function (req, res) {
     getEmailList({ keyword: req.params.keyword}).then(result => {
-        res.send(result)  
+        res.json(result)  
     })
 })
 
 
-app.listen(3001)
-console.log(`Server running on ${chalk.green.bold(`https://localhost:3001`)}`)
+app.listen(3241)
+console.log(`Server running on ${chalk.green.bold(`https://localhost:3241`)}`)

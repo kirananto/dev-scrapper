660e1c981a0425073de371032d83415ec01bfda4
Kiran Anto <kirananto@gmail.com>
2019-09-29 10:04:10 +0530

    Add a way to start a task

--- src/api.ts
+++ src/api.ts
@@ -1,47 +1,54 @@
-import chalk from "chalk"
-import { getKeywords, getRepos, getEmailList } from "./models"
+import chalk from 'chalk'
+import { getKeywords, getRepos, getEmailList } from './models'
+import { scrapeEmails } from './core/scrapeEmails'
+var bodyParser = require('body-parser')
 var cors = require('cors')
-
 const express = require('express')
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(cors())
- 
-app.get('/keywords', function (req, res) {
+
+app.get('/keywords', function(req, res) {
     getKeywords().then(result => {
         res.send(result)
     })
 })
 
-app.get('/keywords/:keyword', function (req, res) {
-    getKeywords({ keyword: req.params.keyword}).then(result => {
+app.post('/keywords', function(req, res) {
+    scrapeEmails(req.body.keywords.split(',').map(item => item.trim()))
+    res.json({ message: "Enqueued the task"})
+})
+
+app.get('/keywords/:keyword', function(req, res) {
+    getKeywords({ keyword: req.params.keyword }).then(result => {
         res.json(result)
     })
 })
 
-app.get('/keywords/:keyword/repos', function (req, res) {
-    getRepos({ keyword: req.params.keyword}).then(result => {
-        res.json(result)  
+app.get('/keywords/:keyword/repos', function(req, res) {
+    getRepos({ keyword: req.params.keyword }).then(result => {
+        res.json(result)
     })
 })
 
-app.get('/keywords/:keyword/repos/:repoId', function (req, res) {
+app.get('/keywords/:keyword/repos/:repoId', function(req, res) {
     getRepos({ keyword: req.params.keyword, repoId: req.params.repoId }).then(result => {
-        res.json(result)  
+        res.json(result)
     })
 })
 
-app.get('/keywords/:keyword/repos/:repoId', function (req, res) {
+app.get('/keywords/:keyword/repos/:repoId', function(req, res) {
     getEmailList({ keyword: req.params.keyword, repoId: req.params.repoId }).then(result => {
-        res.json(result)  
+        res.json(result)
     })
 })
 
-app.get('/keywords/:keyword/emails/', function (req, res) {
-    getEmailList({ keyword: req.params.keyword}).then(result => {
-        res.json(result)  
+app.get('/keywords/:keyword/emails/', function(req, res) {
+    getEmailList({ keyword: req.params.keyword }).then(result => {
+        res.json(result)
     })
 })
-
 
 app.listen(3241)
-console.log(`Server running on ${chalk.green.bold(`https://localhost:3241`)}`)
+console.log(`Server running on ${chalk.green.bold(`http://localhost:3241`)}`)
